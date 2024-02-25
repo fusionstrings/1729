63dc54dc2cf7f421b27866b229ee21c09f52aa93
Dilip Kr. Shukla <fusionstrings@gmail.com>
2024-02-25 01:29:04 +0100

    fix(e2e): fix breaking test

---
+++ source/main.tsx
@@ -0,0 +1,10 @@
+import { HomeDOM } from "#home-dom";
+
+function main() {
+  customElements.define(
+    "fusionstrings-1729",
+    HomeDOM,
+  );
+}
+
+export { main };
--- source/request-handler.ts
+++ source/request-handler.ts
@@ -1,14 +1,24 @@
+import { serveFile } from "#http/file_server";
+import browserImportmap from '#browser-importmap' assert { type: 'json' };
+
+type BrowserAssets = keyof typeof browserImportmap['imports'];
+
 async function requestHandler(request: Request) {
     try {
         const { pathname } = new URL(request.url);
 
         const pathnameHandlerID = pathname.replace('/', '#');
-        const pathnameHandler = pathnameHandlerID === '#' ? "#home" : pathnameHandlerID;
+        const pathnameHandler  = pathnameHandlerID === '#' ? "#home" : pathnameHandlerID;
+        
+        if(pathnameHandler in browserImportmap.imports){
+            return serveFile(request, browserImportmap.imports[pathnameHandler as BrowserAssets])
+        }
 
         const { requestHandlerHTTP } = await import(pathnameHandler);
         return requestHandlerHTTP(request);
     } catch (error) {
         console.error(error.message || error.toString());
+        
         const templateURL = new URL('../www/404.html', import.meta.url).toString();
         const notFound = await fetch(templateURL);
         
